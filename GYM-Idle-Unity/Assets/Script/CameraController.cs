e437c9d87e5cbc1114bce4c10d8810725e903e46
SerTouch0 <sertacbaysal44@gmail.com>
2025-09-16 17:54:17 +0300

    v0.8 Camera Controller Update

--- GYM-Idle-Unity/Assets/Script/CameraController.cs
+++ GYM-Idle-Unity/Assets/Script/CameraController.cs
@@ -1,133 +1,213 @@
 using UnityEngine;
-
+ 
 public class CameraController : MonoBehaviour
+
 {
+
     [Header("Camera Settings")]
+
     public float panSpeed = 2f;
+
     public float zoomSpeed = 3f;
+
     public float minZoom = 8f;
+
     public float maxZoom = 20f;
-    
+ 
     [Header("Movement Limits")]
+
     public float minX = -12f;
+    
     public float maxX = 12f;
+
     public float minZ = -12f;
+
     public float maxZ = 8f;
-    
+ 
     private Vector3 lastTouchPosition;
+
     private Camera cam;
+
     private Vector3 initialPosition;
-    
+ 
     void Start()
+
     {
-       cam = GetComponent<Camera>();
-    initialPosition = transform.position;
-    
-    // Mobil için size ayarı
-    #if UNITY_ANDROID || UNITY_IOS
-    cam.orthographicSize = 12f;
-    #endif
-    }
-    
-    void Update()
-    {
-        // Touch input (mobil için)
-        if (Input.touchCount == 1)
-        {
-            HandleSingleTouch();
-        }
-        else if (Input.touchCount == 2)
-        {
-            HandlePinchZoom();
-        }
-        
-        // Mouse input (test için - editörde)
-        #if UNITY_EDITOR
-        HandleMouseInput();
+
+        cam = GetComponent<Camera>();
+
+        initialPosition = transform.position;
+ 
+        #if UNITY_ANDROID || UNITY_IOS
+
+        cam.orthographicSize = 12f;
+
         #endif
+
     }
-    
+ 
+    void Update()
+
+    {
+
+        if (Input.touchCount == 1)
+
+        {
+
+            HandleSingleTouch();
+
+        }
+
+        else if (Input.touchCount == 2)
+
+        {
+
+            HandlePinchZoom();
+
+        }
+ 
+        #if UNITY_EDITOR
+
+        HandleMouseInput();
+
+        #endif
+
+    }
+ 
     void HandleSingleTouch()
+
     {
+
         Touch touch = Input.GetTouch(0);
-        
+ 
         if (touch.phase == TouchPhase.Began)
+
         {
-            lastTouchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, transform.position.y));
+
+            lastTouchPosition = touch.position;
+
         }
+
         else if (touch.phase == TouchPhase.Moved)
+
         {
-            Vector3 currentTouchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, transform.position.y));
-            Vector3 difference = lastTouchPosition - currentTouchPosition;
-            
-            Vector3 newPosition = transform.position + difference;
-            
-            // Sınırları uygula
+
+            Vector3 delta = cam.ScreenToViewportPoint(lastTouchPosition - (Vector3)touch.position);
+ 
+            Vector3 move = new Vector3(delta.x * panSpeed, 0, delta.y * panSpeed);
+ 
+            Vector3 newPosition = transform.position + move;
+ 
+            // Sınır uygula
+
             newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+
             newPosition.z = Mathf.Clamp(newPosition.z, minZ, maxZ);
-            
+ 
             transform.position = newPosition;
+ 
+            lastTouchPosition = touch.position;
+
         }
+
     }
-    
+ 
     void HandlePinchZoom()
+
     {
+
         Touch touch1 = Input.GetTouch(0);
+
         Touch touch2 = Input.GetTouch(1);
-        
+ 
         Vector2 touch1PrevPos = touch1.position - touch1.deltaPosition;
+
         Vector2 touch2PrevPos = touch2.position - touch2.deltaPosition;
-        
+ 
         float prevTouchDeltaMag = (touch1PrevPos - touch2PrevPos).magnitude;
+
         float touchDeltaMag = (touch1.position - touch2.position).magnitude;
-        
+ 
         float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
-        
+ 
         if (cam.orthographic)
+
         {
+
             cam.orthographicSize += deltaMagnitudeDiff * zoomSpeed * 0.01f;
+
             cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minZoom, maxZoom);
+
         }
+
     }
-    
+ 
     #if UNITY_EDITOR
+
     void HandleMouseInput()
+
     {
-        // Mouse ile pan (test için)
+
         if (Input.GetMouseButtonDown(0))
+
         {
-            lastTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            lastTouchPosition = Input.mousePosition;
+
         }
-        
+ 
         if (Input.GetMouseButton(0))
+
         {
-            Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 difference = lastTouchPosition - currentMousePosition;
-            
-            Vector3 newPosition = transform.position + difference;
+
+            Vector3 delta = cam.ScreenToViewportPoint(lastTouchPosition - Input.mousePosition);
+ 
+            Vector3 move = new Vector3(delta.x * panSpeed, 0, delta.y * panSpeed);
+ 
+            Vector3 newPosition = transform.position + move;
+
             newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+
             newPosition.z = Mathf.Clamp(newPosition.z, minZ, maxZ);
-            
+ 
             transform.position = newPosition;
+ 
+            lastTouchPosition = Input.mousePosition;
+
         }
-        
-        // Mouse scroll ile zoom
+ 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
+
         if (cam.orthographic)
+
         {
+
             cam.orthographicSize -= scroll * zoomSpeed;
+
             cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minZoom, maxZoom);
+
         }
+
     }
+
     #endif
-    
-    // Kamerayı başlangıç pozisyonuna döndür (isteğe bağlı)
+ 
     public void ResetCamera()
+
     {
+
         transform.position = initialPosition;
+
         if (cam.orthographic)
+
         {
+
             cam.orthographicSize = 5f;
+
         }
+
     }
+
 }
+
+ 
